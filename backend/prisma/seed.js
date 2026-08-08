5210790390bcca168db3f7bb6b7d746d01ebb010
SAMEER LOHANI <sameerlohani200510@gmail.com>
2026-08-08 18:54:06 +0530

    Fix prisma client in seed

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,7 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
+const prisma = require('../src/config/db');
 const bcrypt = require('bcrypt');
-
-const prisma = new PrismaClient();
 
 async function main() {
   console.log('Seeding database...');
